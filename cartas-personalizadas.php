75bdb2099e72303b78dc6dcc3ed33c4bb3a8a128
jmenes <menes3d@gmail.com>
2026-06-04 13:07:34 +0200

    chore: bump plugin version to 1.0.2

--- cartas-personalizadas.php
+++ cartas-personalizadas.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Cartas Personalizadas
  * Plugin URI:        https://github.com/jmenes/cartas-personalizadas
  * Description:       Plugin para vender cartas personalizadas con WooCommerce.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            menes.studio
  * Text Domain:       cartas-personalizadas
  * GitHub Plugin URI: jmenes/cartas-personalizadas
